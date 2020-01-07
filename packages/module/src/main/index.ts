e8b791fd64fbc54dcd951cf2ba26695fc644f570
panzer-vor <xyk0279@gmail.com>
2020-01-07 15:36:40 +0800

    p

--- packages/module/src/main/index.ts
+++ packages/module/src/main/index.ts
@@ -1,22 +1,24 @@
-import {Module, register} from "core-fe";
+import { Module, register } from "core-fe";
 import MainComponent from "./component/Main";
-import {State} from "./type";
-
+import { State } from "./type";
+import "antd/es/layout/style";
+import "antd/es/page/style";
+import "antd/es/upload/style";
 const initialState: State = {
-    loading: true,
-    nav: {
-        collapsed: false,
-    },
+  loading: true,
+  nav: {
+    collapsed: false
+  }
 };
 
 class MainModule extends Module<State, {}, {}> {
-    *toggleNav(): any {
-        this.setState({
-            nav: {
-                collapsed: !this.state.nav.collapsed,
-            },
-        });
-    }
+  *toggleNav(): any {
+    this.setState({
+      nav: {
+        collapsed: !this.state.nav.collapsed
+      }
+    });
+  }
 }
 const module = register(new MainModule("main", initialState));
 export const actions = module.getActions();
