ec24f9c5944fc63a48bd79a236070b1df35246ea
xyk0279 <yorkerxu@chancetop.com>
2020-06-12 21:30:59 +0800

    init shop detail

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,14 +5,15 @@ const eleme = require('eleme-openapi-sdk');
 
 class HomeController extends Controller {
   async index() {
-    const { config: { eleToken, eleConfig } } = this;
+    const {
+      config: { eleToken, eleConfig },
+    } = this;
 
     const rpcClient = new eleme.RpcClient(eleToken, eleConfig);
     // 实例化一个服务对象
     const shopService = new eleme.ShopService(rpcClient);
-    shopService.getShop(123456).then(shopInfo => {
-      this.ctx.body = shopInfo;
-    });
+    const shopInfo = await shopService.getShop(123456);
+    this.ctx.body = shopInfo;
   }
 }
 
